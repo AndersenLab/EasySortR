b7b2473a09bc2710c3ccfdf6db28fc2a957b9d75
Tyler Shimko <TShimko126@gmail.com>
2015-05-12 11:31:37 -0500

    Add data and tests directories to project along with testthat functionality

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(SorterDataAssemblr)
+
+test_check("SorterDataAssemblr")
---
+++ tests/testthat/test_summarizePlate_worms.R
@@ -0,0 +1,6 @@
+library(testthat)
+context("Plate summarization")
+
+test_that("column names are correct", {
+    testData <- load("../../data/test1.rda")
+})
