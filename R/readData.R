6a0d2c87ef65fdec1c6a0861bcea0c111dfe02c9
Tyler Shimko <TShimko126@gmail.com>
2015-05-21 13:00:11 -0500

    Update documentation for readData function

--- R/readData.R
+++ R/readData.R
@@ -1,6 +1,7 @@
-#' readData
+#' Read plate data
 #'
-#' Reads sorter data files into R. Built for use exlusively with worms. Depends on COPASutils for the readSorter function.
+#' This is the primary fuction for reading data into R with this package. It is built exlusively for use with worm data. This function can read in individual plates, plates from a single directory (e.g. setup or score directory), or plates from an experimental directory (with both score and setup sub-directories). The files/directories to be read in (individual file/directory/experiment directory) are detected and handled automatically by the function. This function also utilizes an SVM to determine whether objects are more likely to be bubbles or worms and labels them accordingly. It also estimates the lifestage of each worm.
+#' 
 #' @param fileDir The file or directory to be read. This can be an individual file, a directory containing .txt files to be read, or the directory containing the score and setup directories.
 #' @param tofmin The minimum time of flight value allowed. Defaults to 60.
 #' @param tofmin The minimum time of flight value allowed. Defaults to 2000.
