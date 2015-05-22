20b885e0484577dc86df5ccac0ff9166a155dad2
Tyler Shimko <TShimko126@gmail.com>
2015-05-22 15:47:08 -0500

    Add ensureWide to mvPrune

--- R/bamfPrune.R
+++ R/bamfPrune.R
@@ -8,12 +8,13 @@
 #' @export
 
 bamfPrune <- function(data) {
+    data <- ensureLong(data)
     dataWithOutliers <- data %>%
         dplyr::filter(!is.na(strain)) %>%
-        dplyr::group_by(condition, variable)%>%
-        dplyr::summarise(iqr = IQR(value, na.rm = TRUE),
-                         q1 = quantile(value,probs=.25, na.rm = TRUE),
-                         q3 = quantile(value,probs=.75, na.rm = TRUE))%>%
+        dplyr::group_by(condition, trait)%>%
+        dplyr::summarise(iqr = IQR(phenotype, na.rm = TRUE),
+                         q1 = quantile(phenotype ,probs=.25, na.rm = TRUE),
+                         q3 = quantile(phenotype ,probs=.75, na.rm = TRUE))%>%
         dplyr::mutate(cut1h = q3+(iqr*2),
                cut1l =q1-(iqr*2),
                cut2h = q3+(iqr*3),
@@ -26,20 +27,20 @@ bamfPrune <- function(data) {
                cut5h = q3+(iqr*7),
                cut6l = q1-(iqr*10),
                cut6h = q3+(iqr*10))%>%
-        dplyr::left_join(plate, ., by=c("condition", "variable"))%>%
-        dplyr::mutate(onehs = ifelse( cut2h > value & value >= cut1h,1,0),
-               onels = ifelse( cut2l < value & value <= cut1l,1,0),
-               twohs = ifelse( cut3h > value & value >= cut2h,1,0),
-               twols = ifelse( cut3l < value & value <= cut2l,1,0),
-               threehs = ifelse(cut4h > value & value >= cut3h,1,0),
-               threels = ifelse(cut4l < value & value <= cut3l,1,0),
-               fourhs = ifelse(cut5h > value &  value >= cut4h,1,0),
-               fourls = ifelse(cut5l < value &  value <= cut4l,1,0),
-               fivehs = ifelse(cut6h > value & value >= cut5h,1,0),
-               fivels = ifelse(cut6l < value & value <= cut5l,1,0),
-               sixhs = ifelse(value >= cut6h,1,0),
-               sixls = ifelse(value <= cut6l,1,0))%>%
-        dplyr::group_by(condition, variable)%>%
+        dplyr::left_join(plate, ., by=c("condition", "trait"))%>%
+        dplyr::mutate(onehs = ifelse( cut2h > phenotype & phenotype >= cut1h,1,0),
+               onels = ifelse( cut2l < phenotype & phenotype <= cut1l,1,0),
+               twohs = ifelse( cut3h > phenotype & phenotype >= cut2h,1,0),
+               twols = ifelse( cut3l < phenotype & phenotype <= cut2l,1,0),
+               threehs = ifelse(cut4h > phenotype & phenotype >= cut3h,1,0),
+               threels = ifelse(cut4l < phenotype & phenotype <= cut3l,1,0),
+               fourhs = ifelse(cut5h > phenotype &  phenotype >= cut4h,1,0),
+               fourls = ifelse(cut5l < phenotype &  phenotype <= cut4l,1,0),
+               fivehs = ifelse(cut6h > phenotype & phenotype >= cut5h,1,0),
+               fivels = ifelse(cut6l < phenotype & phenotype <= cut5l,1,0),
+               sixhs = ifelse(phenotype >= cut6h,1,0),
+               sixls = ifelse(phenotype <= cut6l,1,0))%>%
+        dplyr::group_by(condition, trait)%>%
         dplyr::mutate(s1h = sum(onehs, na.rm=TRUE),
                s2h = sum(twohs, na.rm=TRUE),
                s3h = sum(threehs, na.rm=TRUE),
@@ -52,7 +53,7 @@ bamfPrune <- function(data) {
                s5l = sum(fivels, na.rm=TRUE),
                s6h = sum(sixhs, na.rm=TRUE),
                s6l = sum(sixls, na.rm=TRUE))%>%
-        dplyr::group_by(condition, variable)%>%
+        dplyr::group_by(condition, trait)%>%
         dplyr::mutate(p1h = ifelse(sum(onehs, na.rm=TRUE)/n()>=.05,1,0),
                p2h = ifelse(sum(twohs, na.rm=TRUE)/n()>=.05,1,0),
                p3h = ifelse(sum(threehs, na.rm=TRUE)/n()>=.05,1,0),
@@ -66,8 +67,8 @@ bamfPrune <- function(data) {
                p5l = ifelse(sum(fivels, na.rm=TRUE)/n()>=.05,1,0),
                p6l = ifelse(sum(sixls, na.rm=TRUE)/n()>=.05,1,0))%>%
         dplyr::mutate(numst = dplyr::n())%>%
-        dplyr::group_by(condition, variable)%>%
-        dplyr::filter(!is.na(variable), !is.na(value), !is.na(iqr), !is.na(q1), !is.na(q3), !is.na(cut1h), !is.na(cut1l), !is.na(cut2h), !is.na(cut2l), !is.na(cut3h), !is.na(cut3l), !is.na(cut4h), !is.na(cut4l), !is.na(cut5l), !is.na(cut5h), !is.na(cut6l), !is.na(cut6h), !is.na(onehs), !is.na(onels), !is.na(twohs), !is.na(twols), !is.na(threehs), !is.na(threels), !is.na(fourhs), !is.na(fourls), !is.na(fivehs), !is.na(fivels), !is.na(sixhs), !is.na(sixls), !is.na(s1h), !is.na(s2h), !is.na(s3h), !is.na(s4h), !is.na(s5h), !is.na(s1l), !is.na(s2l), !is.na(s3l), !is.na(s4l), !is.na(s5l), !is.na(s6h), !is.na(s6l), !is.na(p1h), !is.na(p2h), !is.na(p3h), !is.na(p4h), !is.na(p5h), !is.na(p6h), !is.na(p1l), !is.na(p2l), !is.na(p3l), !is.na(p4l), !is.na(p5l), !is.na(p6l), !is.na(numst)) %>%
+        dplyr::group_by(condition, trait)%>%
+        dplyr::filter(!is.na(trait), !is.na(phenotype), !is.na(iqr), !is.na(q1), !is.na(q3), !is.na(cut1h), !is.na(cut1l), !is.na(cut2h), !is.na(cut2l), !is.na(cut3h), !is.na(cut3l), !is.na(cut4h), !is.na(cut4l), !is.na(cut5l), !is.na(cut5h), !is.na(cut6l), !is.na(cut6h), !is.na(onehs), !is.na(onels), !is.na(twohs), !is.na(twols), !is.na(threehs), !is.na(threels), !is.na(fourhs), !is.na(fourls), !is.na(fivehs), !is.na(fivels), !is.na(sixhs), !is.na(sixls), !is.na(s1h), !is.na(s2h), !is.na(s3h), !is.na(s4h), !is.na(s5h), !is.na(s1l), !is.na(s2l), !is.na(s3l), !is.na(s4l), !is.na(s5l), !is.na(s6h), !is.na(s6l), !is.na(p1h), !is.na(p2h), !is.na(p3h), !is.na(p4h), !is.na(p5h), !is.na(p6h), !is.na(p1l), !is.na(p2l), !is.na(p3l), !is.na(p4l), !is.na(p5l), !is.na(p6l), !is.na(numst)) %>%
         dplyr::mutate(cuts = ifelse(sixhs == 1 & ((s6h + s5h + s4h )/numst) <= .05 & (s5h==0|s4h==0), TRUE,
                              ifelse(sixls == 1 & ((s6l + s5l +s4l)/numst) <= .05 & (s5l==0|s4l==0), TRUE, FALSE)),
                cuts1 = ifelse( ((s6h + s5h + s4h )/numst) >= .05 & s6h>=1&s5h>=1&s4h>=1, FALSE,
--- R/mvPrune.R
+++ R/mvPrune.R
@@ -5,6 +5,7 @@
 #' @export
 
 mvPrune <- function(data, mvCols = c("f.L1", "f.ad", "mean.TOF", "mean.EXT")){
+    data <- ensureWide(data)
     noNAs <- data %>% ungroup() %>% na.omit()
     outlierFrame <- noNAs %>% select_(.dots=mvCols)
     outlierFrameWithOutliers <- outlierFrame %>% mutate(mvOutlier=pcout(outlierFrame, explvar = 0.99, crit.M1 = 0.35, crit.c1=2.5, critM2=0.5, crit.c2= 0.995, outbound = 0.1, cs = 0.45, makeplot = FALSE)$wfinal01)
