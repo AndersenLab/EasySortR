0e683c708771809c298fb1afb0672e214cabf7fe
Tyler Shimko <TShimko126@gmail.com>
2015-06-02 11:46:01 -0500

    Condense summarize function into one function: sumplate

--- R/summarize.R
+++ R/summarize.R
@@ -1,16 +1,3 @@
-#' summarizePlates
-#'
-#' This function summarizes plates when the associated setup plates are not included in the analysis. The function takes the \code{plate} argument and returns a summarized version of the plate, with one well per line. The original plate data must be in wide format.
-#'
-#' @param plate The raw plate data frame to be summarized.
-#' @param strains A character vector of length equal to the number of wells in in the plate being summarized.
-#' @param quantiles Boolean indicating whether or not quantile values (q10, q25, q75, q90) should be included in the summarized data frame. Defaults to FALSE.
-#' @param log Boolean indicating whether or not log-transformed values should be included in the summarized data frame. Defaults to FALSE.
-#' @param ends Boolean indicating whether or not min and max values should be included in the summarized data frame. Defaults to FALSE.
-#' @param long Boolean indicating whether to return the data in long format. Defaults to FALSE.
-#' @importFrom dplyr %>%
-#' @export
-
 summarize_plates <- function(plate, quantiles = FALSE, log = FALSE,
                              ends = FALSE, long = FALSE) {
     plate <- plate[plate$call50 == "object"
@@ -302,7 +289,7 @@ summarize_plates <- function(plate, quantiles = FALSE, log = FALSE,
 }
 
 
-#' Summarize a plate and a add a column for the normalized population
+#' Summarize a plate or multiple plates
 #'
 #' This function takes a list of plates as input and returns a single data frame
 #' with data summarized for each well/condition pair. As input, this function
@@ -311,27 +298,34 @@ summarize_plates <- function(plate, quantiles = FALSE, log = FALSE,
 #' in the list should be a data frame consisting of all of the setup plates to
 #' be summarized, from which the number sorted to each well will be extracted.
 #'
-#' @param plates A list consisting of all of the score plates in the first
-#' element and the setup plates in the second elements.
+#' @param plates A single data frame of all data or a list consisting of all of
+#' the score plates in the first element and the setup plates in the second
+#' elements.
 #' @param quantiles Boolean indicating whether or not quantile values (q10, q25,
 #' q75, q90) should be included in the summarized data frame. Defaults to FALSE.
 #' @param log Boolean indicating whether or not log-transformed values should be
 #' included in the summarized data frame. Defaults to FALSE.
 #' @param ends Boolean indicating whether or not min and max values should be
 #' included in the summarized data frame. Defaults to FALSE.
+#' @param long Boolean stating whether to output the data in long format.
 #' @importFrom dplyr %>%
 #' @export
 
-summarize_sorted <- function(plates, quantiles=FALSE, log=FALSE,
-                             ends=FALSE){
-    score <- summarize_plates(plates[[1]], quantiles, log, ends) %>%
-        dplyr::arrange(plate, row, col)
-    setup <- plates[[2]] %>%
-        dplyr::group_by(date, experiment, round, assay,
-                        plate, drug, row, col) %>% dplyr::summarize(
-                            control.n.sorted = sum(sort == 6))
-    data <- dplyr::left_join(score, setup) %>%
-        dplyr::mutate(norm.n = n / control.n.sorted) %>%
-        dplyr::select(-n.sorted, -control.n.sorted)
+sumplate <- function(plates, quantiles = FALSE, log = FALSE,
+                             ends = FALSE, long = FALSE){
+    if (length(plates) == 2) {
+        score <- summarize_plates(plates[[1]], quantiles, log, ends, long) %>%
+            dplyr::arrange(plate, row, col)
+        setup <- plates[[2]] %>%
+            dplyr::group_by(date, experiment, round, assay,
+                            plate, drug, row, col) %>% dplyr::summarize(
+                                control.n.sorted = sum(sort == 6))
+        data <- dplyr::left_join(score, setup) %>%
+            dplyr::mutate(norm.n = n / control.n.sorted) %>%
+            dplyr::select(-n.sorted, -control.n.sorted)
+    } else {
+        score <- summarize_plates(plates, quantiles, log, ends, long) %>%
+            dplyr::arrange(plate, row, col)
+    }
     return(data)
 }
