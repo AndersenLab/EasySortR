0578aa9802ffd7afcba97dc1e425492e41c3c35a
Tyler Shimko <TShimko126@gmail.com>
2015-06-09 09:32:11 -0500

    Comment regression code and change if/else to tryCatch to catch mismatched column numbers

--- R/regress.R
+++ R/regress.R
@@ -8,19 +8,40 @@
 #' @export
 
 regress <- function(dataframe, assay=FALSE){
+    
+    # Ensure the data frame is in long format, then filter out all NA strains
+    # and nonfinite (NA, NaN, Inf) values
+    
     dataframe <- ensure_long(dataframe)
+    dataframe <- dplyr::filter(dataframe, is.finite(phenotype), !is.na(strain))
+    
+    # Get rid of n.sorted phenotype values (always 0)
+    
     dataframe <- dataframe %>%
         dplyr::filter(trait != "n.sorted")
     data <- dataframe %>%
+        
+    # Separate the data from the controls into two different data frames
+        
             dplyr::filter(!is.na(control))
     controls <- dataframe %>%
                 dplyr::filter(is.na(control))
     controls$control <- controls$condition
     moltendata <- data
+    
+    # Summarize the controls in case there are replicate controls
+    
     moltencontrols <- controls %>% dplyr::group_by(strain, control, trait) %>%
         dplyr::summarize(controlphenotype = mean(phenotype, na.rm=TRUE))
+    
+    # Join the control values back to the controls
+    
     fusedmoltendata <- dplyr::left_join(moltendata, moltencontrols,
                                  by=c("strain", "control", "trait"))
+    
+    # Perform the regression step with or without the assay values and pull out
+    # the residuals with the broom package
+    
     if(assay){
         modeldata <- fusedmoltendata %>%
             dplyr::group_by(condition, trait) %>%
@@ -33,21 +54,32 @@ regress <- function(dataframe, assay=FALSE){
             dplyr::filter(!is.na(phenotype), !is.na(controlphenotype)) %>%
             dplyr::do(broom::augment(lm(phenotype ~ controlphenotype, data=.)))
     }
+    
+    # Select only the information that we're interested in and rename .resid to
+    # just resid
+    
     resids <- modeldata %>%
         dplyr::select(condition, trait, phenotype, controlphenotype, .resid) %>%
         dplyr::rename(resid = .resid)
+    
+    # Arrange the columns so that they align for a cbind
+    
     arrangedmolten <- dplyr::arrange(fusedmoltendata, condition, trait, phenotype,
                               controlphenotype)
     arrangedresids <- dplyr::arrange(resids, condition, trait, phenotype,
                               controlphenotype)
     
+    # Make sure that the number of rows between the two arranged data frames are
+    # the same and that the values for the matching columns are all the same
     
-    if (all.equal(arrangedmolten[,c(5,11,12,16)], arrangedresids[,-5])) {
-        regressedframe <- cbind(arrangedmolten, arrangedresids$resid) %>%
-            dplyr::rename(resid = `arrangedresids$resid`)
-    } else {
-        stop("The values for the raw data (when arranged on 'condition', 'trait', 'phenotype', and 'controlphenotype' do not match up with same values in the residual data frame output by the broom package's augment function. Quitting to prevent binding of incorrect data")
-    }
+    tryCatch({
+        if (all.equal(arrangedmolten[,c(5,11,12,16)], arrangedresids[,-5])) {
+            regressedframe <- cbind(arrangedmolten, arrangedresids$resid) %>%
+                dplyr::rename(resid = `arrangedresids$resid`)
+        } else {
+            stop("The values for the raw data (when arranged on 'condition', 'trait', 'phenotype', and 'controlphenotype' do not match up with same values in the residual data frame output by the broom package's augment function. Quitting to prevent binding of incorrect data")
+        }
+    }, error = function(e) stop("The number of rows in arrangedmolten and arrangedresids do not match."))
     
     return(regressedframe)
 }
