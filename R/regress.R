bd9511af4df3866e397baeffd5b63aea369d6fb2
Tyler Shimko <TShimko126@gmail.com>
2015-08-11 14:11:22 -0500

    Group by assay for control summarization and regression

--- R/regress.R
+++ R/regress.R
@@ -11,77 +11,77 @@
 regress <- function(dataframe, assay=FALSE){
     # Ensure the data frame is in long format, then filter out all NA strains
     # and nonfinite (NA, NaN, Inf) values
-    
+
     dataframe <- ensure_long(dataframe)
     dataframe <- dplyr::filter(dataframe, is.finite(phenotype), !is.na(strain))
-    
+
     # Get rid of n.sorted phenotype values (always 0)
-    
+
     dataframe <- dataframe %>%
         dplyr::filter(trait != "n.sorted")
-    
+
     # Perform the regression step with or without the assay values and pull out
     # the residuals with the broom package
-    
+
     if(assay){
-        
+
         # Remove condition only present in one assay, otherwise regression will
         # fail
-        
+
         dataframe <- dataframe %>%
             dplyr::group_by(condition) %>%
             dplyr::filter(length(unique(assay)) > 1)
-        
+
         # Get the model data
         # `deparse` and `substitute` are necessary to get the formula to work
         # inside of a call to `mutate`, see:
         # http://stackoverflow.com/questions/28776792/combining-dplyrdo-with-dplyrmutate
-        
+
         withresids <- dataframe %>%
             dplyr::group_by(condition, trait) %>%
             dplyr::mutate(resid = residuals(lm(deparse(substitute(
                 phenotype ~ assay - 1)))))
-        
+
         regressedframe <- withresids %>%
             dplyr::mutate(phenotype = resid) %>%
             dplyr::select(-resid)
-        
+
     } else {
         # Separate the data from the controls into two different data frames
-        
+
         data <- dataframe %>%
             dplyr::filter(!is.na(control))
         controls <- dataframe %>%
             dplyr::filter(is.na(control))
         controls$control <- controls$condition
         moltendata <- data
-        
+
         # Summarize the controls in case there are replicate controls
-        
-        moltencontrols <- controls %>% dplyr::group_by(strain, control, trait) %>%
+
+        moltencontrols <- controls %>% dplyr::group_by(strain, control, trait, assay) %>%
             dplyr::summarize(controlphenotype = mean(phenotype, na.rm=TRUE))
-        
+
         # Join the control values back to the controls
-        
+
         fusedmoltendata <- dplyr::left_join(moltendata, moltencontrols,
-                                            by=c("strain", "control", "trait")) %>%
+                                            by=c("strain", "control", "trait", "assay")) %>%
             dplyr::filter(!is.na(phenotype), !is.na(controlphenotype))
-        
+
         # Get the model data
         # `deparse` and `substitute` are necessary to get the formula to work
         # inside of a call to `mutate`, see:
         # http://stackoverflow.com/questions/28776792/combining-dplyrdo-with-dplyrmutate
-        
+
         withresids <- fusedmoltendata %>%
             dplyr::group_by(condition, trait) %>%
             dplyr::mutate(resid = residuals(lm(deparse(substitute(
                 phenotype ~ controlphenotype - 1)))))
-        
+
         regressedframe <- withresids %>%
             dplyr::mutate(phenotype = resid) %>%
             dplyr::select(-resid, -controlphenotype)
-        
+
     }
-    
+
     return(regressedframe)
 }
